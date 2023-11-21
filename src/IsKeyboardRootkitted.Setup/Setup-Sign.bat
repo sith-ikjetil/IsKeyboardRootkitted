f927c885c6a6cb662fd1fa95280ea5855e4d4c8d
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2023-11-21 09:20:39 +0100

    updated code signing setup.

--- src/IsKeyboardRootkitted.Setup/Setup-Sign.bat
+++ src/IsKeyboardRootkitted.Setup/Setup-Sign.bat
@@ -1,2 +1,2 @@
 ECHO CREATING DIGITAL SIGNATURE
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\signtool" sign /sm /sha1 85208471365034134761c4326c082209b103d6d6 /t "http://timestamp.comodoca.com/authenticode" .\bin\Release\KKS.IsKeyboardRkt.Setup.msi
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\signtool" sign /sm /sha1 1FD9BD05B67F9490D630145169668F8192448335 /t "http://timestamp.sectigo.com" .\bin\Release\IsKeyboardRootkitted.Setup.msi
