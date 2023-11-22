456512395202b10a39528e26771649a780337616
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2023-11-22 16:45:28 +0100

    Updated Setup-Sign.bat.

--- src/IsKeyboardRootkitted.App/Setup-Sign.bat
+++ src/IsKeyboardRootkitted.App/Setup-Sign.bat
@@ -1,2 +1,2 @@
 ECHO CREATING DIGITAL SIGNATURE
-"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\signtool"  sign /sm /a /fd sha256 /t  "http://timestamp.comodoca.com/authenticode" .\..\x64\Release\KKS.IsKeyboardRktApp.exe
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.18362.0\x64\signtool"  sign /sm /a /fd sha256 /sha1 1FD9BD05B67F9490D630145169668F8192448335 /t "http://timestamp.sectigo.com" .\..\x64\Release\IsKeyboardRootkitted.App.exe
